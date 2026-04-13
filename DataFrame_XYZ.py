a5e1e0466595843922ca736bd7a9819a065989de
michalmonka92 <monkamicha@gmail.com>
2026-04-13 11:46:54 +0200

    Update DataFrame_XYZ.py

--- DataFrame_XYZ.py
+++ DataFrame_XYZ.py
@@ -729,7 +729,7 @@ with st.expander("Energies", expanded=False):
             margin-left: -10px;
             margin-right: 0px;
             margin-bottom: -590px; /* Trik, żeby 'podłożyć' tło pod wykres */
-            height: 350px;
+            height: 380px;
         ">
         </div>
     """, unsafe_allow_html=True)
