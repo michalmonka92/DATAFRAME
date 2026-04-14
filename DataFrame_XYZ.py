bfd352f1f6ee6dbb86fd7b6fc4ef36244f838eb5
michalmonka92 <monkamicha@gmail.com>
2026-04-14 13:18:55 +0200

    Update DataFrame_XYZ.py

--- DataFrame_XYZ.py
+++ DataFrame_XYZ.py
@@ -90,7 +90,7 @@ kolor_ramki = "#ff9300"  # Twój kolor (np. niebieski)
 kolor_tla = "#363636"    # Jasny odcień dla wypełnienia
 pomarancz = "#ff9300"
 
-st.markdown(f"""<hr style="height:5px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)   
+st.markdown(f"""<hr style="height:5px;margin-top: -1px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)   
 
 
 st.markdown(f"""
@@ -139,7 +139,7 @@ with cola:
         <h4>Linker modifications</h4>
     </div>
     """, unsafe_allow_html=True)  
-        st.markdown(f"""<hr style="height:5px;margin-top: -30px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)
+        st.markdown(f"""<hr style="height:5px;margin-top: -10px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)
         image = Image.open('linkers.jpg')
         st.image(image, use_container_width=True)     
 
@@ -164,7 +164,7 @@ with colb:
         <h4>Substituents</h4>
     </div>
     """, unsafe_allow_html=True)
-        st.markdown(f"""<hr style="height:5px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)
+        st.markdown(f"""<hr style="height:5px;margin-top: -10px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)
 
         image = Image.open('Subs.jpg')
         st.image(image, use_container_width=True)      
