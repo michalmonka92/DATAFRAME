b7583689147eb3a422aef9c0ab0af0831b60c0c2
michalmonka92 <monkamicha@gmail.com>
2026-04-14 13:12:31 +0200

    Update DataFrame_XYZ.py

--- DataFrame_XYZ.py
+++ DataFrame_XYZ.py
@@ -90,7 +90,7 @@ kolor_ramki = "#ff9300"  # Twój kolor (np. niebieski)
 kolor_tla = "#363636"    # Jasny odcień dla wypełnienia
 pomarancz = "#ff9300"
 
-st.markdown("""<hr style="height:5px; border:none; color:#ff9300; background-color:#ff9300;" />""", unsafe_allow_html=True)   
+st.markdown("""<hr style="height:5px; border:none; color:{pomarancz}; background-color:{pomarancz};" />""", unsafe_allow_html=True)   
 
 
 st.markdown(f"""
