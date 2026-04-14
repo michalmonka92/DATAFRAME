3602a459b9d21b68a2e62960ee2f292a663028c6
michalmonka92 <monkamicha@gmail.com>
2026-04-14 15:38:51 +0200

    Update DataFrame_XYZ.py

--- DataFrame_XYZ.py
+++ DataFrame_XYZ.py
@@ -100,7 +100,7 @@ pomarancz = "#ff9300"
 st.markdown("""<style>
     /* Celujemy w tekst nagłówka expandera */
     .stExpander details summary p {
-        color: yellow !important;
+        color: green !important;
         font-weight: bold;
         font-size: 1.1rem;}
     </style>
